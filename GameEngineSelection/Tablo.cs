8bba953949bb15c6bbfd737b0411ae9a196b9307
Hasan Talha Yalmanbaş <hasan.yalmanbas@roketworks.com>
2020-08-09 01:27:21 +0300

    Fix some bugs and change certificate

--- GameEngineSelection/Tablo.cs
+++ GameEngineSelection/Tablo.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GameEngineSelection
@@ -46,26 +39,45 @@ namespace GameEngineSelection
             }
         }
 
+        public bool IsNumeric(string text)
+        {
+            double test;
+            Console.WriteLine(text);
+            return double.TryParse(text, out test);
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
-          for (int i = 0; i < table_Row.Items.Count; i++)
+
+
+            for (int i = 0; i < table_Row.Items.Count; i++)
             {
                 for (int j = 0; j < table_Column.Items.Count; j++)
                 {
                     if (dataGridView1.Rows[i].Cells[j].Value != null)
                     {
+                        if (!IsNumeric(dataGridView1.Rows[i].Cells[j].Value.ToString()))
+                        {
+                            MessageBox.Show("Lütfen Tablodaki Tüm Hücrelerde Sadece Sayı Kullanın!");
+                            return;
+                        }
+
                         Values[i, j] = double.Parse(dataGridView1.Rows[i].Cells[j].Value.ToString());
-                    }else
+                    }
+                    else
                     {
-                        MessageBox.Show("Lütfen Tablodaki Tüm Hücreleri Doldurun.");
+                        MessageBox.Show("Lütfen Tablodaki Tüm Hücreleri Doldurun!");
                         return;
                     }
-                    
+
                 }
             }
             MessageBox.Show("Tablo Kaydedildi.");
             isSaveTable = true;
         }
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -73,7 +85,7 @@ namespace GameEngineSelection
             {
                 if (isSaveTable)
                 {
-                    Sonuc sonuc = new Sonuc(comboBox1.GetItemText(comboBox1.SelectedItem),Values,dataGridView1 ,table_Row.Items.Count,table_Column.Items.Count);
+                    Sonuc sonuc = new Sonuc(comboBox1.GetItemText(comboBox1.SelectedItem), Values, dataGridView1, table_Row.Items.Count, table_Column.Items.Count);
                     sonuc.ShowDialog();
                     this.Close();
 
